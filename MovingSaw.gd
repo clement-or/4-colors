198f126de61b7cee30e89b2ab056046db29e37df
Topy <topy72.mine@gmail.com>
2019-08-25 17:18:55 +0200

    Piques et scies blancs

--- MovingSaw.gd
+++ MovingSaw.gd
@@ -6,8 +6,8 @@ onready var follow = $PathFollow2D
 
 # Exports
 export var speed = 0.5 #between 0 and 1
-export(int, "RED", "GREEN", "BLUE", "YELLOW") var color
-enum {RED,GREEN,BLUE,YELLOW}
+export(int, "RED", "GREEN", "BLUE", "YELLOW", "WHITE") var color
+enum {RED,GREEN,BLUE,YELLOW,WHITE}
 
 var is_moving_right = true
 
@@ -30,7 +30,12 @@ func _process(delta):
 		is_moving_right = !is_moving_right
 
 func _draw():
-	var line_color = c.COLORS[color]
+	var line_color
+	if color != WHITE:
+		line_color = c.COLORS[color]
+	else:
+		line_color = Color(1,1,1)
+	
 	var points = curve.get_baked_points()
 	var nb_points = curve.get_point_count()
 	
