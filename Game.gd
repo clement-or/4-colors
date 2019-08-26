ff05dd4d422e6c1a261db7573944ea91a9c29619
Topy <topy72.mine@gmail.com>
2019-08-26 10:42:24 +0200

    Revert "Merge branch 'dev' into level-design-3"
    This reverts commit fecc81cef7f5fcee5d4e9b3052eadaadd6559b65.

--- Game.gd
+++ Game.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-var LEVELS = [
+const LEVELS = [
 	preload("res://levels/Level0.tscn"),
 	preload("res://levels/Level1.tscn"),
 	preload("res://levels/Level2.tscn"),
-	preload("res://levels/Level3.tscn"),
-	preload("res://levels/Level4.tscn"),
-	preload("res://levels/Level5.tscn"),
-	preload("res://levels/Level6.tscn"),
-	preload("res://levels/Level7.tscn"),
-	preload("res://levels/Level8.tscn"),
-	preload("res://levels/Level9.tscn")
+	preload("res://levels/Level3.tscn")
 ]
 var current_level_nb = 0
 var current_level_inst
@@ -25,13 +19,8 @@ func load_level(nb):
 	current_level_inst = LEVELS[nb].instance()
 	add_child(current_level_inst)
 	current_level_inst.connect("level_finished", self, "next_level")
-	current_level_inst.connect("level_restarted", self, "restart_current_level")
 
 func next_level():
 	call_deferred("remove_child",current_level_inst)
 	current_level_nb += 1
 	call_deferred("load_level",current_level_nb)
-
-func restart_current_level():
-	current_level_nb -= 1
-	next_level()
